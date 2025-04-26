6e2cbf973ad40d1bf4ecbe0c9143892d127d2ddb
Tiến Lê <21110677@student.hcmute.edu.vn>
2025-04-26 20:10:32 +0700

    Refactor Analysis Data Retrieval to Include Candidates
    - Updated the get_data function in analysis.py to return open jobs along with associated candidates, improving the data structure for better client-side consumption.
    - Removed unnecessary variables and streamlined the logic for fetching candidates, ensuring only relevant data is returned.
    - Enhanced the response model by excluding internal identifiers from the job and candidate objects for cleaner output.

--- server/app/routes/analysis.py
+++ server/app/routes/analysis.py
@@ -7,27 +7,28 @@ router = APIRouter(prefix="/analysis", tags=["analysis"])
 
 @router.get("/data", response_model=[])
 async def get_data():
-    open_jobs = []
+    open_jobs_with_candidates = []
+    open_jobs = jobs_collection.find({"status": "open"})
+    open_jobs = await open_jobs.to_list(length=100)
 
-    # Get open jobs
-    data_open_jobs = jobs_collection.find({
-        "status": "open"
-    })
-    
-    data_open_jobs = await data_open_jobs.to_list(length=100)
+    if len(open_jobs) == 0:
+        return open_jobs_with_candidates
 
-    if len(data_open_jobs) == 0:
-        return open_jobs
-
-    # Get candidate with status new in job open
-    for open_job in data_open_jobs:
-        print(open_job)
+    for open_job in open_jobs:
+        open_job.pop('_id', None)
+        
         candidates = candidates_collection.find({
-            "job_id": open_job['id']
+            "job_id": open_job['id'],
+            "status": "New"
         })
         candidates = await candidates.to_list(length=100)
+        if len(candidates) == 0:
+            continue
+        
+        for candidate in candidates:
+            candidate.pop('_id', None)
 
-        open_jobs.append({
+        open_jobs_with_candidates.append({
             "id": open_job['id'],
             "name": open_job['title'],
         	"field": open_job['department'],
@@ -38,7 +39,7 @@ async def get_data():
             "candidates": candidates
         })
 
-    return open_jobs
+    return open_jobs_with_candidates
     
 
     
