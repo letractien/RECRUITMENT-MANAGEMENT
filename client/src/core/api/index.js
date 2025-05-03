c131f69f2ef9bc6497c353a7006199da1264ed2c
Tiến Lê <21110677@student.hcmute.edu.vn>
2025-05-03 21:21:38 +0700

    Remove switchToMongoDB script and clean up API exports
    - Deleted the switchToMongoDB.js script, which was responsible for configuring the client to use a MongoDB backend.
    - Updated index.js to remove unused imports for candidate, job, and interview services, streamlining the API exports.

--- client/src/core/api/index.js
+++ client/src/core/api/index.js
@@ -1,11 +1,5 @@
 import apiService from './apiClient';
-import * as candidateService from './candidate.service';
-import * as jobService from './job.service';
-import * as interviewService from './interview.service';
 
 export {
   apiService,
-  candidateService,
-  jobService,
-  interviewService
 }; 
